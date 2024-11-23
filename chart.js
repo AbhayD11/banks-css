786d3de6a6d42cbd63cee2e715bf31b335c90048
AbhayD11 <77774758+AbhayD11@users.noreply.github.com>
2024-11-24 01:17:17 +0530

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,24 @@
+function drawBalanceDoughnutChart(chartId, percentages, colors) {
+    const chartData = {
+      labels: [],
+      datasets: [{
+        data: percentages,
+        backgroundColor: colors,
+      }]
+    };
+  
+    const options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: { display: false }
+      },
+      cutout: '80%' 
+    };
+  
+    const canvas = document.getElementById(chartId);
+    if (canvas) {
+      new Chart(canvas.getContext('2d'), { type: 'doughnut', data: chartData, options });
+    }
+  }
+  
